3ab691522061f76b6caecb63866f43e3712583cb
kiran6055 <kirandevopskumar@gmail.com>
2023-09-19 19:33:55 +0530

    kk

--- vpc/main.tf
+++ vpc/main.tf
@@ -87,7 +87,7 @@ resource "aws_route_table_association" "database" {
 resource "aws_security_group" "web" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = output.vpc_id
 
   ingress {
     description      = "TLS from VPC"
