f34a08cb8f8a7ecb64a80d13039917a9453c347a
kiran6055 <kirandevopskumar@gmail.com>
2023-09-19 19:08:35 +0530

    kk

--- vpc/main.tf
+++ vpc/main.tf
@@ -68,12 +68,12 @@ resource "aws_nat_gateway" "main" {
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
-  tags = var.tags
+  
 }
 
 
 resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
-  tags = var.tags
+ 
 }
