7915f7a60b9ea3e226eba041be291df74c493424
root <root@ip-172-31-40-113.ap-south-1.compute.internal>
2024-02-21 09:25:43 +0000

    all

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,10 +1,10 @@
-output "vpc_id" {
-  value = aws_vpc.timing.id
-}
+#output "vpc_id" {
+#  value = aws_vpc.timing.id
+#}
 
-output "db_subnet_group_name" {
-  value = aws_db_subnet_group.database.name
-}
+#output "db_subnet_group_name" {
+#  value = aws_db_subnet_group.database.name
+#}
 
 
 output "public_subnet_ids" {
